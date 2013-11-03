60d36a0c22742e459d7226090f30bf1b1fef5e8f
Cator V <catorwei@gmail.com>
2013-11-03 23:41:38 +0800

    copy_htmlencode 和 htmldecode 函数算法改进

--- src/html.c
+++ src/html.c
@@ -41,38 +41,16 @@ string_t striptags(string_t *str)
 
 string_t copy_htmlencode(const string_t str, bool skip_quotes)
 {
-  string_t result;
-  size_t len = 0;
-  string_t s = str;
-  string_t p;
+  const size_t len = strlen(str);
+  const string_t se = str + len;
+  register string_t s = str;
+  register string_t p;
   
-  while (*s != '\0') {
-    switch (*s) {
-      case '&':
-        len += 5;
-        break;
-        
-      case '"':
-      case '\'':
-        len += skip_quotes ? 1 : 6;
-        break;
-        
-      case '<':
-      case '>':
-        len += 4;
-        break;
-        
-      default:
-        len++;
-    }
-    s++;
-  }
-  
-  result = (string_t)malloc(sizeof(char) * (len + 1));
+  string_t result = (string_t)malloc(sizeof(char) * (len * 6 + 1));
   
   s = str;
   p = result;
-  while (*s != '\0') {
+  while (s < se) {
     switch (*s) {
       case '&':
         memcpy(p, "&amp;", 5);
@@ -82,8 +60,7 @@ string_t copy_htmlencode(const string_t str, bool skip_quotes)
       case '"':
       case '\'':
         if (skip_quotes) {
-          *p = *s;
-          p++;
+          *p++ = *s;
         } else {
           memcpy(p, *s == '"' ? "&quot;" : "&apos;", 6);
           p += 6;
@@ -97,8 +74,7 @@ string_t copy_htmlencode(const string_t str, bool skip_quotes)
         break;
        
       default:
-        *p = *s;
-        p++;
+        *p++ = *s;
         break;
     }
     s++;
@@ -109,44 +85,48 @@ string_t copy_htmlencode(const string_t str, bool skip_quotes)
   return result;
 }
 
+typedef struct htmlentity_t {
+  string_t entity;
+  size_t length;
+  unsigned char c1;
+  unsigned char c2;
+} htmlentity_t;
+
 string_t htmldecode(string_t *str)
 {
   string_t s = *str;
   string_t p = s;
   
+  htmlentity_t htmlentities[] = {
+    {"&amp;",  5, 0, '&'},
+    {"&quot;", 6, 0, '"'},
+    {"&apos;", 6, 0, '\''},
+    {"&lt;",   4, 0, '<'},
+    {"&gt;",   4, 0, '>'}
+  };
+  int htmlentity_count = sizeof(htmlentities) / sizeof(htmlentity_t);
+  
+  int i;
+  htmlentity_t entity;
+  
   while (*s != '\0') {
     if (*s == '&') {
-      if (strncasecmp(s, "&amp;", 5) == 0) {
-        *p = '&';
-        s += 5;
-        p++;
-        continue;
-      } else if (strncasecmp(s, "&quot;", 6) == 0) {
-        *p = '"';
-        s += 6;
-        p++;
-        continue;
-      } else if (strncasecmp(s, "&apos;", 6) == 0) {
-        *p = '\'';
-        s += 6;
-        p++;
-        continue;
-      } else if (strncasecmp(s, "&lt;", 4) == 0) {
-        *p = '<';
-        s += 4;
-        p++;
-        continue;
-      } else if (strncasecmp(s, "&gt;", 4) == 0) {
-        *p = '>';
-        s += 4;
-        p++;
+      for (i = 0; i < htmlentity_count; i++) {
+        entity = htmlentities[i];
+        if (strncasecmp(s, entity.entity, entity.length) == 0) {
+          if (entity.c1 != 0) {
+            *p++ = entity.c1;
+          }
+          *p++ = entity.c2;
+          s += entity.length;
+          break;
+        }
+      }
+      if (i < htmlentity_count) {
         continue;
       }
     }
-    
-    *p = *s;
-    s++;
-    p++;
+    *p++ = *s++;
   }
   
   *p = '\0';
